d02d6000c5fb59e46f53b3e7642fc39d262aae76
jaehyun0306 <fexis1594@gmail.com>
2023-07-07 16:38:31 +0900

    add RaceController, Race, Region, SearchKeyword, RaceMapper, RaceServie, RaceMapper docs events

---
+++ src/main/java/com/spring/dto/Region.java
@@ -0,0 +1,19 @@
+package com.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Region {
+	private int region_id;
+	private String region_state;
+	private String region_city;
+	private String region_district;
+}
