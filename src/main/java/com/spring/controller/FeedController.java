03f6d8c13a0f967c5f6363bfc1d02700115be252
hansol1227 <akqkrdl1227@gmail.com>
2023-07-17 09:32:52 +0900

    Add - 피드에 사진 첨부기능 (미완성)

--- src/main/java/com/spring/controller/FeedController.java
+++ src/main/java/com/spring/controller/FeedController.java
@@ -4,10 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.spring.dto.FeedPicture;
 import com.spring.dto.PersonalFeed;
+import com.spring.service.FeedPictureService;
 import com.spring.service.FeedService;
 
 @Controller
@@ -15,7 +21,10 @@ public class FeedController {
 	private static final Logger logger = LoggerFactory.getLogger(FeedController.class);
 	
 	@Autowired
-	private FeedService service;
+	private FeedService feedservice;
+	
+	@Autowired
+	private FeedPictureService pictureservice;
 	
 	// 피드 작성 get
 	 @GetMapping("/mypage/write")
@@ -26,11 +35,32 @@ public class FeedController {
 	
 	// 피드 작성 post
 	 @PostMapping("/mypage/write")
-	 public String feedWrite(PersonalFeed pf) throws Exception {
+	 public String feedWrite(@ModelAttribute PersonalFeed pf,
+			 					@RequestParam("file") MultipartFile file,
+			 					Model model) throws Exception {
 	  logger.info("feed write");
 	  
-	  service.insertFeed(pf);
+	  String view = "error";
 	  
-	  return "redirect:/";
-	 }
+	  boolean feedResult = false;
+	  boolean fileResult = false;
+	  
+	  try {
+		  feedResult = feedservice.insertFeed(pf);
+		  
+		  int pfeed = pf.getFeed_id();
+		  
+		  fileResult = pictureservice.insertFeedPicture(file, pfeed);
+		  
+		  if(feedResult && fileResult) {
+			  view = "redirect:/main";
+			  return view;
+		  }
+	  } catch (Exception e) {
+		e.printStackTrace();
+		return view;
+	  	}	  
+	  
+	  return view;
+	}
 }
